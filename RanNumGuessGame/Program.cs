9ec7179fa06db492f2a833c33b98a77be4ae37d3
Mirko Cukich <mirkocukich@gmail.com>
2018-11-18 19:00:03 -0600

    Final update

--- RanNumGuessGame/Program.cs
+++ RanNumGuessGame/Program.cs
@@ -19,7 +19,7 @@ namespace RanNumGuessGame
             var subtraction = randomNumber.Next(2, 10);
 
             int answer = firstNumber * secondNumber - subtraction;
-            string prompt = "\nDon't type in the answer, just press Enter when ready";
+            string prompt = "\nDon't type in the answer, just press Enter when ready\n";
 
             Console.WriteLine("Think of a number between 1 and 10." + prompt);
             Console.ReadKey();
